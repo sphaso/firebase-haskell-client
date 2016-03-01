adb8da7f8881487adc76e8188528bfa0ad8da71d
sphaso <sphaso@gmail.com>
2016-03-01 21:32:04 +0100

    cleaned fireGet function

--- src/Http.hs
+++ src/Http.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Http where
 
 import Control.Lens
 import Data.Aeson.Lens (_String, key)
 import qualified Data.ByteString.Lazy as BL
-import Network.Wreq
+import Network.Wreq as WR (get, responseBody)
 import Network.Wreq.Types
 import Data.Text (pack)
 
-fireGet :: String -> IO ()
-fireGet url = do
-                response <- get url
-                Prelude.print $ (response ^. responseBody)
-
+get :: String -> IO ()
+get url = do
+            response <- WR.get url
+            Prelude.print $ (response ^. WR.responseBody)
 
