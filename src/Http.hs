f91e2ed23e86c418e5a0cbd744aaae5967d5fe68
sphaso <sphaso@gmail.com>
2016-03-04 20:17:09 +0100

    added DELETE

--- src/Http.hs
+++ src/Http.hs
@@ -5,7 +5,7 @@ module Http where
 import Control.Lens
 import Data.Aeson.Lens (_String, key)
 import qualified Data.ByteString.Lazy as BL
-import Network.Wreq as WR (get, responseBody, responseStatus, statusCode)
+import Network.Wreq as WR (get, delete, responseBody, responseStatus, statusCode)
 import Network.Wreq.Types
 import Data.Text (pack)
 import Types
@@ -19,3 +19,7 @@ get (FireRequest url opts) = do
                                 response <- WR.get (url ++ "?" ++ pars)
                                 return $ FireResponse (response ^. WR.responseBody) (response ^. WR.responseStatus . WR.statusCode)
 
+delete :: String -> IO FireResponse
+delete url = do
+                response <- WR.delete url
+                return $ FireResponse (response ^. WR.responseBody) (response ^. WR.responseStatus . WR.statusCode)
