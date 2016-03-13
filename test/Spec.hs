1e65ae409c715848af05f9d585a1f3cadd8a2424
Roland Brand <roland@forschig.ch>
2016-03-13 16:49:26 +0100

    added a first test with HSpec, not working yet (see comments in test/Spec.hs)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception
+import Http
+import Types
+
+-- TODO this is not working yet. Also in the repl, if put the following:
+-- response = get $ FireRequest "http://httpbin.org/xml" []
+-- response has the type of IO FireResponse, and I can't figure out how to access its body or status.
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+	describe "Http.get" $ do
+		it "returns the contens of httpbin.org/xml" $ do
+			(get $ FireRequest "http://httpbin.org/xml" []).status `shouldBe` 200
