2ae32e7cf9dc9f80ae5083578bbd0ab012a31799
Roland Brand <roland@forschig.ch>
2016-03-22 20:03:20 +0100

    just some cleanup

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,12 +4,10 @@ import Control.Exception
 import Http
 import Types
 
--- TODO this is not working yet. Also in the repl, if put the following:
--- response = get $ FireRequest "http://httpbin.org/xml" []
--- response has the type of IO FireResponse, and I can't figure out how to access its body or status.
-
 main :: IO ()
 main = hspec $ do
+
+    -- ### INTEGRATION TESTS ###
     describe "Http.get" $ do
         it "returns the contens of httpbin.org/xml" $ do
             (FireResponse _ status _) <- get $ FireRequest "http://httpbin.org/xml" []
