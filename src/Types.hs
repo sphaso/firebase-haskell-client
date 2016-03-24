19925a1faf07c40572377ad8e96cd80f7ebd75c4
Roland Brand <roland@forschig.ch>
2016-03-24 20:26:05 +0100

    added JSON FireRequest Type Constructor

--- src/Types.hs
+++ src/Types.hs
@@ -8,5 +8,6 @@ type RequestParameter = (QueryParameter, String)
 data QueryParameter = Auth | Shallow | Callback | Format | OrderBy | LimitToFirst | LimitToLast | StartAt | EndAt | EqualTo | Print | Download deriving (Eq, Show)
 
 data FireRequest = FireRequest { url :: String, parameters :: [RequestParameter] }
+                 | JSONFireRequest { json :: String, url :: String,  parameters :: [RequestParameter] }
 
 data FireResponse = FireResponse { body :: ByteString, status :: Int, headers :: [Header] }
