88ef97443fb623e7d05af8f5f8a9059d9c128c2c
sphaso@gmail.com <sphaso@gmail.com>
2016-03-24 20:59:47 +0100

    added POST and validated DELETE

--- src/FireLogic.hs
+++ src/FireLogic.hs
@@ -9,16 +9,29 @@ onlyShallow opts | (shallowIn && (length opts) == 1) || (not shallowIn) = True
                  | otherwise = error "Shallow must be the only parameter"
                     where shallowIn = elem Shallow (map fst opts)
 
-onlyGets :: [RequestParameter] -> Bool
-onlyGets opts | and (map (\x -> elem (fst x) getOnly) opts) = True
-              | otherwise = error "Some parameters are not GET parameters"
-                    where getOnly = [Shallow, Callback, Format, Download, Print]
+-- This is just... wrong!
+--onlyGets :: [RequestParameter] -> Bool
+--onlyGets opts | and (map (\x -> elem (fst x) getOnly) opts) = True
+--              | otherwise = error "Some parameters are not GET parameters"
+--                    where getOnly = [Shallow, Callback, Format, Download, Print]
+
+-- Can orderBy and limits be used with POST \ PUT etc.?
+onlyGet :: [RequestParameter] -> Bool
+onlyGet opts | or (map (\x -> elem (fst x) getOnly) opts) = True
+             | otherwise = error "Some parameters are only GET parameters"
+                   where getOnly = [Shallow, Callback, Format, Download]
 
 printValue :: [RequestParameter] -> Bool
 printValue opts | (isNothing a) || (snda == "pretty") || (snda == "silent") = True
                 | otherwise = error "Print argument not recognized: it should be either pretty or silent"
                     where a = find (\(a,b) -> a == Print) opts
                           snda = snd (fromMaybe (Print, "") a)
+
+printValueSilent :: [RequestParameter] -> Bool
+printValueSilent opts | (isNothing a) || (snda == "pretty") = True
+                      | otherwise = error "Print argument not recognized or not allowed for this verb"
+                           where a = find (\(a,b) -> a == Print) opts
+                                 snda = snd (fromMaybe (Print, "") a)
 
 orderByValue :: [RequestParameter] -> Bool
 orderByValue opts | (isNothing a) || (snda == "$key") || (snda == "$value") || (snda == "$priority") = True
@@ -30,6 +43,12 @@ anyEmpty :: [RequestParameter] -> Bool
 anyEmpty opts | and $ map (\(a,b) -> b /= "") opts = True
               | otherwise = error "Some arguments are empty"
 
-validate :: [RequestParameter] -> Bool
-validate xs = all ($ xs) [anyEmpty, orderByValue, printValue, onlyGets, onlyShallow]
+validateGet :: [RequestParameter] -> Bool
+validateGet xs = all ($ xs) [anyEmpty, orderByValue, printValue, onlyShallow]
+
+validatePost :: [RequestParameter] -> Bool
+validatePost xs = all ($ xs) [anyEmpty, onlyGet, printValue]
+
+validateDelete :: [RequestParameter] -> Bool
+validateDelete xs = all ($ xs) [anyEmpty, printValueSilent]
 
--- src/Http.hs
+++ src/Http.hs
@@ -3,9 +3,11 @@
 module Http where
 
 import Control.Lens
+import Data.Aeson
 import Data.Aeson.Lens (_String, key)
 import qualified Data.ByteString.Lazy as BL
-import Network.Wreq as WR (get, delete, responseBody, responseStatus, statusCode, responseHeaders)
+import Network.Wreq as WR (get, post, delete, 
+                           responseBody, responseStatus, statusCode, responseHeaders)
 import Network.Wreq.Types
 import Data.Text (pack)
 import Types
@@ -14,12 +16,21 @@ import UriBuilder
 
 get :: FireRequest -> IO FireResponse
 get (FireRequest url opts) = do
-                                let validation = validate opts
+                                let validation = validateGet opts
                                 let pars = buildParameters opts
                                 response <- WR.get (url ++ "?" ++ pars)
                                 return $ FireResponse (response ^. WR.responseBody) (response ^. WR.responseStatus . WR.statusCode) (response ^. WR.responseHeaders)
 
-delete :: String -> IO FireResponse
-delete url = do
-                response <- WR.delete url
-                return $ FireResponse (response ^. WR.responseBody) (response ^. WR.responseStatus . WR.statusCode) (response ^. WR.responseHeaders)
+post :: FireRequest -> IO FireResponse
+post (JSONFireRequest json url opts) = do
+                                        let validation = validatePost opts
+                                        let pars = buildParameters opts
+                                        response <- WR.post (url ++ "?" ++ pars) (toJSON json)
+                                        return $ FireResponse (response ^. WR.responseBody) (response ^. WR.responseStatus . WR.statusCode) (response ^. WR.responseHeaders)
+
+delete :: FireRequest -> IO FireResponse
+delete (FireRequest url opts) = do
+                                    let validation = validateDelete opts
+                                    let pars = buildParameters opts
+                                    response <- WR.delete (url ++ "?" ++ pars)
+                                    return $ FireResponse (response ^. WR.responseBody) (response ^. WR.responseStatus . WR.statusCode) (response ^. WR.responseHeaders)
