3f73b27e1e8f0de2fcbdf180c0ee192030659789
Roland Brand <roland@forschig.ch>
2016-05-10 21:39:41 +0200

    first draft of a JWT integration test, INCOMPLETE

--- test/IntegrationTests.hs
+++ test/IntegrationTests.hs
@@ -10,6 +10,7 @@ import Types
 import UriBuilder
 import Auth
 import FireLogic
+import JWT -- not yet existing, will provide the generateJWT function.
 
 spec :: Spec
 spec = do
@@ -28,4 +29,13 @@ spec = do
             let url = (config !! 1) ++ "haskell.json"
             let authReq = (Auth, Prelude.head config)
             FireResponse body _ _ <- get $ FireRequest url [authReq]
+            unpack body `shouldBe` "\"awesome\""
+
+    -- # JWT Module
+    describe "Call Firebase with authentication token" $ do
+        it "return -awesome- when asked for Haskell" $ do
+            config <- getConf
+            let url = (config !! 1) ++ "haskell.json"
+            let secret = (Auth, Prelude.head config)
+            FireResponse body _ _ <- get $ FireRequest url [generateJWT secret ] -- the payload itself (body) will possibly also have to go into the generateJWT function
             unpack body `shouldBe` "\"awesome\""
