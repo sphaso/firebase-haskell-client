74f523562816a03109265dcf37d2bc35580635df
Giovanni <sphaso@gmail.com>
2016-05-17 21:36:19 +0200

    Cleaned generation

--- src/TokenGenerator.hs
+++ src/TokenGenerator.hs
@@ -8,17 +8,8 @@ import Web.JWT (encodeSigned, secret, JWTClaimsSet, JSON, Algorithm(HS256))
 makeClaim :: Int -> [(String, String)] -> FireJWT
 makeClaim iat d = FireJWT 0 iat d Nothing Nothing Nothing Nothing
 
-addNbf :: Int -> FireJWT -> FireJWT
-addNbf nbf (FireJWT v iat d _ exp admin debug) = FireJWT v iat d (Just nbf) exp admin debug
-
-addExp :: Int -> FireJWT -> FireJWT
-addExp exp (FireJWT v iat d nbf _ admin debug) = FireJWT v iat d nbf (Just exp) admin debug
-
-addAdmin :: Bool -> FireJWT -> FireJWT
-addAdmin admin (FireJWT v iat d nbf exp _ debug) = FireJWT v iat d nbf exp (Just admin) debug
-
-addDebug :: Bool -> FireJWT -> FireJWT
-addDebug debug (FireJWT v iat d nbf exp admin _) = FireJWT v iat d nbf exp admin (Just debug)
+makeWholeClaim :: Int -> [(String, String)] -> Maybe Int -> Maybe Int -> Maybe Bool -> Maybe Bool -> FireJWT
+makeWholeClaim iat d nbf exp admin debug = FireJWT 0 iat d nbf exp admin debug
 
 generate :: String -> JWTClaimsSet -> JSON
 generate scrt claims = encodeSigned HS256 (secret $ pack scrt) claims
