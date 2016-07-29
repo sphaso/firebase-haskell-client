de58eddf0e46ce6d0c7cd2c0e09a2a7216662214
Roland Brand <Roland Brand>
2016-07-29 21:02:38 +0200

    switched JWT library to jose-hs

--- src/TokenGenerator.hs
+++ src/TokenGenerator.hs
@@ -3,19 +3,19 @@ module TokenGenerator where
 import Types
 
 import Data.Text (unpack, pack, Text)
-import Web.JWT (def, StringOrURI, stringOrURI, encodeSigned, secret, ClaimsMap, JWTClaimsSet(..), JSON, Algorithm(HS256))
+--import Web.JWT (def, StringOrURI, stringOrURI, encodeSigned, secret, ClaimsMap, JWTClaimsSet(..), JSON, Algorithm(HS256))
 
-makeClaim :: Int -> [(String, String)] -> FireJWT
-makeClaim iat d = FireJWT 0 iat d Nothing Nothing Nothing Nothing
+--makeClaim :: Int -> [(String, String)] -> FireJWT
+--makeClaim iat d = FireJWT 0 iat d Nothing Nothing Nothing Nothing
 
-makeWholeClaim :: Int -> [(String, String)] -> Maybe Int -> Maybe Int -> Maybe Bool -> Maybe Bool -> FireJWT
-makeWholeClaim iat d nbf exp admin debug = FireJWT 0 iat d nbf exp admin debug
+--makeWholeClaim :: Int -> [(String, String)] -> Maybe Int -> Maybe Int -> Maybe Bool -> Maybe Bool -> FireJWT
+--makeWholeClaim iat d nbf exp admin debug = FireJWT 0 iat d nbf exp admin debug
 
-generate :: String -> JWTClaimsSet -> JSON
-generate scrt claims = encodeSigned HS256 (secret $ pack scrt) claims
+--generate :: String -> JWTClaimsSet -> JSON
+--generate scrt claims = encodeSigned HS256 (secret $ pack scrt) claims
 
 --TODO: JWTClaimsSet and ClaimsMap constructors not in scope (solution needed or change approach)
-claimsFromFireJWT :: FireJWT -> JWTClaimsSet
-claimsFromFireJWT (FireJWT v iat d nbf exp admin debug)
-    = def { iss = stringOrURI (pack "Foo") }
+--claimsFromFireJWT :: FireJWT -> JWTClaimsSet
+--claimsFromFireJWT (FireJWT v iat d nbf exp admin debug)
+    -- = def { iss = stringOrURI (pack "Foo") }
         -- where d = insert "admin" admin $ insert "debug" debug . empty
