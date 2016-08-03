9605e539a904a0d3a1beffa071226e007a5e8c2f
Giovanni <sphaso@gmail.com>
2016-08-03 20:58:02 +0200

    moved to JOSE

--- src/TokenGenerator.hs
+++ src/TokenGenerator.hs
@@ -3,19 +3,25 @@ module TokenGenerator where
 import Types
 
 import Data.Text (unpack, pack, Text)
+import Crypto.JWT (ClaimsSet)
 --import Web.JWT (def, StringOrURI, stringOrURI, encodeSigned, secret, ClaimsMap, JWTClaimsSet(..), JSON, Algorithm(HS256))
 
---makeClaim :: Int -> [(String, String)] -> FireJWT
---makeClaim iat d = FireJWT 0 iat d Nothing Nothing Nothing Nothing
+makeClaim :: Int -> [(String, String)] -> FireJWT
+makeClaim iat d = FireJWT 0 iat d Nothing Nothing Nothing Nothing
 
---makeWholeClaim :: Int -> [(String, String)] -> Maybe Int -> Maybe Int -> Maybe Bool -> Maybe Bool -> FireJWT
---makeWholeClaim iat d nbf exp admin debug = FireJWT 0 iat d nbf exp admin debug
+makeWholeClaim :: Int -> [(String, String)] -> Maybe Int -> Maybe Int -> Maybe Bool -> Maybe Bool -> FireJWT
+makeWholeClaim iat d nbf exp admin debug = FireJWT 0 iat d nbf exp admin debug
 
---generate :: String -> JWTClaimsSet -> JSON
+--generate :: String -> ClaimsSet -> JSON
 --generate scrt claims = encodeSigned HS256 (secret $ pack scrt) claims
 
 --TODO: JWTClaimsSet and ClaimsMap constructors not in scope (solution needed or change approach)
---claimsFromFireJWT :: FireJWT -> JWTClaimsSet
---claimsFromFireJWT (FireJWT v iat d nbf exp admin debug)
-    -- = def { iss = stringOrURI (pack "Foo") }
-        -- where d = insert "admin" admin $ insert "debug" debug . empty
+claimsFromFireJWT :: FireJWT -> ClaimsSet
+claimsFromFireJWT (FireJWT v iat d nbf exp admin debug)
+       = emptyClaimsSet
+            & claimIat .~ iat
+            & claimNbf .~ nbf
+            & addClaim "v" v
+            & addClaim "d" d
+            & addClaim "admin" (Bool admin)
+            & addClaim "debug" (Bool debug)
