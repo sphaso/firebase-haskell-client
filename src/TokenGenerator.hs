678abb48147b898d5e2561802d1424ca5bcf61bf
Giovanni <sphaso@gmail.com>
2016-08-11 21:04:33 +0200

    fixed a few type errors

--- src/TokenGenerator.hs
+++ src/TokenGenerator.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module TokenGenerator where
 
 import Types
 
+import Data.Bool
 import Data.Text (unpack, pack, Text)
-import Crypto.JWT (ClaimsSet)
+import Data.Aeson hiding ((.=))
+import Data.Time (UTCTime, NominalDiffTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
+import Control.Lens
+
+import Crypto.JWT
 --import Web.JWT (def, StringOrURI, stringOrURI, encodeSigned, secret, ClaimsMap, JWTClaimsSet(..), JSON, Algorithm(HS256))
 
 makeClaim :: Int -> [(String, String)] -> FireJWT
@@ -15,13 +23,21 @@ makeWholeClaim iat d nbf exp admin debug = FireJWT 0 iat d nbf exp admin debug
 --generate :: String -> ClaimsSet -> JSON
 --generate scrt claims = encodeSigned HS256 (secret $ pack scrt) claims
 
+maybeToBool :: Maybe Bool -> Bool
+maybeToBool (Just a) = a
+maybeToBool Nothing = False
+
+maybeToInt :: Maybe Int -> Int
+maybeToInt (Just a) = a
+maybeToInt Nothing = 0
+
 --TODO: JWTClaimsSet and ClaimsMap constructors not in scope (solution needed or change approach)
 claimsFromFireJWT :: FireJWT -> ClaimsSet
 claimsFromFireJWT (FireJWT v iat d nbf exp admin debug)
        = emptyClaimsSet
-            & claimIat .~ iat
-            & claimNbf .~ nbf
+            & claimIat .~ (realToFrac iat)
+            & claimNbf .~ (realToFrac $ maybeToInt nbf)
             & addClaim "v" v
             & addClaim "d" d
-            & addClaim "admin" (Bool admin)
-            & addClaim "debug" (Bool debug)
+            & addClaim "admin" (Bool (maybeToBool admin))
+            & addClaim "debug" (Bool (maybeToBool debug))
